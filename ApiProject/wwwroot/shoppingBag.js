6d20a6e904e42fa29718d26cb3353eb6954bdb02
Rivky <213836612@PC5-23>
2022-12-29 14:14:38 +0200

    delete orderItem written but not tried because we can not catch template

--- ApiProject/wwwroot/shoppingBag.js
+++ ApiProject/wwwroot/shoppingBag.js
@@ -1,13 +1,14 @@
 
-drawOrderItem = (product) => {
-    var temp = document.getElementById("temp - row")
+drawOrderItem = (product, productIndex) => {
+    let d = document.getElementById("g")
+    alert(d.innerHTML)
     //var clone = temp.content.cloneNode(true)
     //clone.querySelector(".itemName").innerText = product.name
     //clone.querySelector(".itemNumber").innerText = product.id
     ////clone.querySelector(".description").innerText = product.description
     ////clone.querySelector(".price").innerText = product.price
     ////clone.querySelector(".image").src = `../img/${product.imgUrl}`
-    ///*clone.querySelector("button").addEventListener("click", () => addToBag(product))*/
+    ///*clone.querySelector(".DeleteButton").addEventListener("click", () => deleteItem(productIndex))*/
     //document.body.appendChild(clone)
 }
 
@@ -19,12 +20,31 @@ drawOrderItem = (product) => {
 //        <td class="totalColumn delete"><div class="expandoHeight" style="height: 99px;"><p class="price"></p><a href="#" title="לחצו כאן כדי להסיר את פריט זה" class="Hide DeleteButton showText">הסרת פריט</a></div></td>
 //    </tr>
 //</template>
+removeOrderItems = () => {
+    let orderItems = document.getElementsByClassName("item-row");
+    for (var i = orderItems.length - 1; i >= 0; i--) {
+        document.body.removeChild(orderItems[i])
+    }
 
+}
+deleteItem = (productIndex) => {
+    let orderJson = sessionStorage.getItem("bag")
+    let order = JSON.parse(orderJson)
+    let updatedOrder = []
+    for (var i = 0; i < order.length; i++) {
+        if (i != productIndex)
+            updatedOrder.push(order[i])
+    }
+    updatedOrder = JSON.stringify(updatedOrder)
+    sessionStorage.setItem("bag", updatedOrder)
+    removeOrderItems()
+    drawOrder()
+}
 drawOrder = () => {
     let orderJson = sessionStorage.getItem("bag")
     let order = JSON.parse(orderJson)
     for (var i = 0; i < order.length; i++) {
-        drawOrderItem(order[i])
+        drawOrderItem(order[i], i)
     }
 
 }
@@ -58,7 +78,7 @@ placeOrder = () => {
         orderItems.push(orderItem)
         totalPrice += order[i].price
     }
-    let userId = JSON.parse( sessionStorage.getItem("details"))[0].id
+    let userId = JSON.parse(sessionStorage.getItem("details"))[0].id
     let orderObject = {
         "userId": userId,
         "date": new Date(),
@@ -68,7 +88,7 @@ placeOrder = () => {
     let orderString = JSON.stringify(orderObject)//
     localStorage.setItem("order", orderString)//
     serverCall(orderObject)
-}   
+}
 
 
 window.addEventListener("load", drawOrder())
